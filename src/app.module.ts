12b303a2e7090237937bc0fc6861056edc22a36b
Sudeep <sudeeppprajapati@gmail.com>
2026-02-08 15:12:07 +0530

    Setup PostgreSQL connection using TypeORM with environment config and DB connectivity check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { HelloModule } from './hello/hello.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbTestService } from './db-test.service';
 
 @Module({
-  imports: [HelloModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [DbTestService],
 })
 export class AppModule { }
