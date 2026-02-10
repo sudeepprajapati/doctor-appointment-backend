dd4747272679dd63917d9512c8256be33f31a4fa
Sudeep Prajapati <92222733+sudeepprajapati@users.noreply.github.com>
2026-02-10 18:09:30 +0530

    Update role-based Google OAuth flow and refine doctor/patient entity (#7)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,32 @@
-import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import express from 'express';
+import { GoogleAuthGuard } from './guards/google-auth-guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    // Patient login
-    @Get('google/patient')
-    @UseGuards(AuthGuard('google'))
-    googlePatientLogin() { }
+    // Frontend calls this when user clicks Doctor / Patient
+    @Get('select-role')
+    selectRole(
+        @Query('role') role: 'PATIENT' | 'DOCTOR',
+        @Res() res: express.Response,
+    ) {
+        if (!['PATIENT', 'DOCTOR'].includes(role)) {
+            return res.status(400).json({ message: 'Invalid role' });
+        }
 
-    // Doctor login
-    @Get('google/doctor')
-    @UseGuards(AuthGuard('google'))
-    googleDoctorLogin() { }
+        return res.redirect(`/auth/google?state=${role}`);
+    }
 
-    // Google redirects
+    @Get('google')
+    @UseGuards(GoogleAuthGuard)
+    googleLogin() { }
+
+    //Google OAuth callback
     @Get('google/callback')
     @UseGuards(AuthGuard('google'))
     async googleAuthRedirect(@Req() req) {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
